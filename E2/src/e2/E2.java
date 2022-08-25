ed4dc44b6c98fd939c55c320eebb730a2afcf415
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-08-25 02:09:03 -0500

    Simple questionnaire using JOptionPane
    showMessageDialog y showInputDialog

---
+++ E2/src/e2/E2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package e2;
+
+/**
+ *
+ * @author maria
+ */
+public class E2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        quest c = new quest();
+        c.setVisible(true);
+        c.setResizable(false);
+        c.setTitle("Menu");
+        c.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
