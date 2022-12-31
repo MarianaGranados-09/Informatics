e9771fb6e79c1b391fe2970ff734aad0f9c01f49
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-12-31 03:15:23 -0600

    second term exam

---
+++ EXAMEN2PARCIAL/src/examen2parcial/EXAMEN2PARCIAL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2parcial;
+
+/**
+ *
+ * @author maria
+ */
+public class EXAMEN2PARCIAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio i = new Inicio();
+        i.setVisible(true);
+        i.setResizable(false);
+        i.setTitle("Inicio El Pirata");
+        i.setLocationRelativeTo(null);
+    }
+    
+}
