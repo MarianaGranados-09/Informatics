f85f1b19d6b58836caf28adcd4f8d154db0e25c7
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-01-22 03:59:22 -0600

    static variables and methods

--- Tv.java
+++ Tv.java
@@ -10,24 +10,36 @@
 public class Tv {
     public static void main(String[] args){
         Tv tv1 = new Tv();
+        Tv tv2 = new Tv();
+        
         tv1.turnOn();
+        tv2.turnOn();
+        
         tv1.setChannel(30);
         tv1.setVolume(3);
         
         System.out.println("Tv1's channel is "+tv1.channel);
+        System.out.println("Number of Tvs on: "+numberOfTvs);
     }
         int channel = 1; //default channel
         int volumeLevel = 1; //default
         boolean on = false; //tv is off
+        
+        static int numberOfTvs = 0; //all instances share data with static variables
+        //also known as class variables
         
         public Tv(){
         
         }
         public void turnOn(){
             on = true;
+            numberOfTvs++;
         }
         public void turnOff(){
             on = false;
+        }
+        static int getNumberOfTvs(){
+            return numberOfTvs;
         }
         public void setChannel(int newChannel){
             if(on && newChannel >= 1 && newChannel <= 120){
