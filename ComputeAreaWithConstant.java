ecf1cf35fccb4cca449d2a22dff5ab282d27c06e
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-01-22 01:34:36 -0600

    using constants with final keyword

---
+++ ComputeAreaWithConstant.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mariana
+ */
+import java.util.Scanner; 
+
+public class ComputeAreaWithConstant {
+    public static void main(String[] args){
+        //named constant or constant represents data that never
+        //changes syntax: final datatype CONSTANTNAME = value
+        final double PI = 3.14159;
+        System.out.println("The value of PI is "+PI);
+    }
+    
+}
